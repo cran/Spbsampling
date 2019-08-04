dfa818824f52e95dcb40aaf034abc78ff444f09f
Francesco Pantalone <pantalone.fra@gmail.com>
2019-08-04 11:10:02 +0000

    version 1.3.0

--- R/Spbsampling.R
+++ R/Spbsampling.R
@@ -1,7 +1,7 @@
 #' Spatially balanced sampling designs
 #'
 #' Selection of spatially balanced samples. In particular, the implemented
-#' sampling designs allow to select probability samples spread over the
+#' sampling designs allow to select probability samples well spread over the
 #' population of interest, in any dimension and using any distance function
 #' (e.g. Euclidean distance, Manhattan distance). The implementation has been
 #' done in \code{C++} through the use of \code{Rcpp} and \code{RcppArmadillo}.
@@ -10,11 +10,13 @@
 #'
 #' Maintainer: Francesco Pantalone \email{pantalone.fra@@gmail.com}
 #' @references
-#' Benedetti R, Piersimoni F (2017). “A spatially balanced design with
-#' probability function proportional to the within sample distance.”
-#' \emph{Biometrical Journal}, \strong{59}(5), 1067–1084.
+#' Benedetti R, Piersimoni F (2017). A spatially balanced design with
+#' probability function proportional to the within sample distance.
+#' \emph{Biometrical Journal}, \strong{59}(5), 1067-1084.
+#' \url{https://doi.org/10.1002/bimj.201600194}
 #'
-#' Benedetti R, Piersimoni F (2017). “Fast Selection of Spatially Balanced Samples.” \emph{arXiv}.
+#' Benedetti R, Piersimoni F (2017). Fast Selection of Spatially Balanced Samples. \emph{arXiv}.
+#' \url{https://arxiv.org/abs/1710.09116}
 #' @docType package
 #' @name Spbsampling
 #'
--- src/stsum.cpp
+++ src/stsum.cpp
@@ -4,7 +4,7 @@
 //' Standardize a symmetric matrix (distances) to fixed row (column) totals
 //'
 //' Standardizes a distance matrix to fixed rows and columns
-//' products. The function iteratively constrains the rows sums of the matrix
+//' totals. The function iteratively constrains the rows sums of the matrix
 //' to know totals, and in order to keep the symmetry of the matrix, at each
 //' iteration performs an average with its transpose. When the known totals are
 //' all equal to a constant (e.g. 1), this method provides a simple and
@@ -19,15 +19,16 @@
 //' @param  vec A vector of row (column) constraints.
 //' @param differ A scalar with the maximum accepted difference with the constraint (default = 1e-15).
 //' @param niter An integer with the maximum number of iterations (default = 1000).
-//' @return Return a standardized distance matrix of size NxN.
+//' @return Returns a standardized distance matrix of size NxN.
 //' @references
-//' Benedetti R, Piersimoni F (2017). “A spatially balanced design with
-//' probability function proportional to the within sample distance.”
-//' \emph{Biometrical Journal}, \strong{59}(5), 1067–1084.
+//' Benedetti R, Piersimoni F (2017). A spatially balanced design with
+//' probability function proportional to the within sample distance.
+//' \emph{Biometrical Journal}, \strong{59}(5), 1067-1084.
+//' \url{https://doi.org/10.1002/bimj.201600194}
 //' @examples
 //' dis <- as.matrix(dist(cbind(simul2$x, simul2$y))) # distance matrix
 //' con <- rep(1, nrow(dis)) # vector of constraints
-//' stand_dist <- stsum(dis, con) # standardized matrix
+//' stand_dist <- stsum(mat = dis, vec = con) # standardized matrix
 //' @export
 // [[Rcpp::export]]
 
